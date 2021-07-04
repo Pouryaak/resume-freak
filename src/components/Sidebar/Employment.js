3ea56e55fe90c895fae073f696e3996605b874ca
Pouryaak <pourya.ak1996@gmail.com>
2021-07-05 00:44:31 +0800

    adding emplyment history feature

--- src/components/Sidebar/Employment.js
+++ src/components/Sidebar/Employment.js
@@ -11,18 +11,105 @@ import TextField from "@material-ui/core/TextField";
 import "./Employment.css";
 import { TextArea } from "semantic-ui-react";
 import SaveIcon from "@material-ui/icons/Save";
+import { useDispatch, useSelector } from "react-redux";
+import { addEmpHistory, selectDetails } from "../../features/detailSlice";
 
 function Employment() {
-  const [emps, setEmps] = useState(["emp1"]);
+  const [emps, setEmps] = useState([]);
+  const [companyName, setComapnyName] = useState("");
+  const [position, setPosition] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [technologies, setTechnologies] = useState("");
+  const [description, setDescription] = useState("");
+
+  const empHistory = useSelector(selectDetails);
+  const dispatch = useDispatch();
 
   const addMoreEmp = () => {
-    setEmps((prev) => {
-      return [...prev, `emp${prev.length}`];
-    });
+    // console.log({
+    //   empId: `emp${empHistory.employmentHistory.length + 1}`,
+    //   company: companyName,
+    //   position,
+    //   startDate,
+    //   endDate,
+    //   technologies,
+    //   description,
+    // });
+    dispatch(
+      addEmpHistory({
+        id: `emp${empHistory.employmentHistory.length + 1}`,
+        company: companyName,
+        position,
+        startDate,
+        endDate,
+        technologies,
+        description,
+      })
+    );
+    // setEmps((prev) => {
+    //   return [...prev, `emp${prev.length}`];
+    // });
   };
+  console.log(`emp${empHistory.employmentHistory.length + 1}`);
 
   return (
     <div>
+      <div className="employment__fields">
+        <div className="sideHeader__fields">
+          <TextField
+            fullWidth
+            label="Company Name"
+            variant="outlined"
+            value={companyName}
+            onChange={(e) => setComapnyName(e.target.value)}
+          />
+        </div>
+        <div className="sideHeader__fields">
+          <TextField
+            fullWidth
+            label="Position"
+            variant="outlined"
+            value={position}
+            onChange={(e) => setPosition(e.target.value)}
+          />
+        </div>
+        <div className="sideHeader__fields">
+          <TextField
+            fullWidth
+            label="Start Date"
+            variant="outlined"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+        </div>
+        <div className="sideHeader__fields">
+          <TextField
+            fullWidth
+            label="End Date"
+            variant="outlined"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+        </div>
+        <div className="sideHeader__fields">
+          <TextField
+            fullWidth
+            label="Technologies Used"
+            variant="outlined"
+            value={technologies}
+            onChange={(e) => setTechnologies(e.target.value)}
+          />
+        </div>
+        <div className="sideHeader__fields">
+          <TextArea
+            placeholder="Describe your tasks and responsibilities!"
+            style={{ minHeight: 200, minWidth: 435 }}
+            value={description}
+            onChange={(e, { value }) => setDescription(value)}
+          />
+        </div>
+      </div>
       <Fab
         color="secondary"
         aria-label="add"
@@ -30,65 +117,89 @@ function Employment() {
         style={{ marginBottom: "20px" }}
         onClick={addMoreEmp}
       >
-        <AddIcon /> Add More Emplyment History
+        <AddIcon /> Add
       </Fab>
-      {emps.map((emp, i) => (
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls={`panel${i}a-content`}
-            id={`panel${i}a-header`}
-          >
-            Employment {i + 1}
-          </AccordionSummary>
-          <AccordionDetails>
-            <div className="employment__fields">
-              <div className="sideHeader__fields">
-                <TextField fullWidth label="Company Name" variant="outlined" />
+
+      {empHistory.employmentHistory &&
+        empHistory.employmentHistory.map((emp, i) => (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel${i}a-content`}
+              id={`panel${i}a-header`}
+            >
+              Employment {i + 1}
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className="employment__fields">
+                <div className="sideHeader__fields">
+                  <TextField
+                    fullWidth
+                    label="Company Name"
+                    variant="outlined"
+                    value={emp.company}
+                  />
+                </div>
+                <div className="sideHeader__fields">
+                  <TextField
+                    fullWidth
+                    label="Position"
+                    variant="outlined"
+                    value={emp.position}
+                  />
+                </div>
+                <div className="sideHeader__fields">
+                  <TextField
+                    fullWidth
+                    label="Start Date"
+                    variant="outlined"
+                    value={emp.startDate}
+                  />
+                </div>
+                <div className="sideHeader__fields">
+                  <TextField
+                    fullWidth
+                    label="End Date"
+                    variant="outlined"
+                    value={emp.endDate}
+                  />
+                </div>
+                <div className="sideHeader__fields">
+                  <TextField
+                    fullWidth
+                    label="Technologies Used"
+                    variant="outlined"
+                    value={emp.technologies}
+                  />
+                </div>
+                <div className="sideHeader__fields">
+                  <TextArea
+                    placeholder="Describe your tasks and responsibilities!"
+                    style={{ minHeight: 200, minWidth: 400 }}
+                    value={emp.description}
+                  />
+                </div>
+                <div className="sideHeader__fields">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<DeleteIcon />}
+                  >
+                    Delete
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<SaveIcon />}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    Update
+                  </Button>
+                </div>
               </div>
-              <div className="sideHeader__fields">
-                <TextField fullWidth label="Position" variant="outlined" />
-              </div>
-              <div className="sideHeader__fields">
-                <TextField fullWidth label="Start Date" variant="outlined" />
-              </div>
-              <div className="sideHeader__fields">
-                <TextField fullWidth label="End Date" variant="outlined" />
-              </div>
-              <div className="sideHeader__fields">
-                <TextField
-                  fullWidth
-                  label="Technologies Used"
-                  variant="outlined"
-                />
-              </div>
-              <div className="sideHeader__fields">
-                <TextArea
-                  placeholder="Describe your tasks and responsibilities!"
-                  style={{ minHeight: 200, minWidth: 400 }}
-                />
-              </div>
-              <div className="sideHeader__fields">
-                <Button
-                  variant="contained"
-                  color="primary"
-                  startIcon={<DeleteIcon />}
-                >
-                  Delete
-                </Button>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  startIcon={<SaveIcon />}
-                  style={{ marginLeft: "10px" }}
-                >
-                  Update
-                </Button>
-              </div>
-            </div>
-          </AccordionDetails>
-        </Accordion>
-      ))}
+            </AccordionDetails>
+          </Accordion>
+        ))}
     </div>
   );
 }
